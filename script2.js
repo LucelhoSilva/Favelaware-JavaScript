053a46f0b44407fe2fc564638747f7abdf560ce9
Lucelho Cristiano <lucelhocristiano28@gmail.com>
2022-12-05 18:21:34 -0300

    Aula 2
    1. Declare uma variável com seu nome e uma com seu CPF usando let e const ?
    
    2. Qual das opções abaixo é a correta sobre concatenação ?

---
+++ script2.js
@@ -0,0 +1,20 @@
+/*
+1. Declare uma variável com seu nome e uma com seu CPF usando let e const ?
+*/
+
+let meuNome = "fulano"
+console.log("Me chamo"+meuNome)
+
+const cpf = "000.000.000-00"
+console.log("Meu cpf "+cpf)
+
+/*
+2. Qual das opções abaixo é a correta sobre concatenação?
+a) console.log('Me chamo'+ meu nome)
+b) console.log('Meu nome + meuNome')
+c) console.log('Meu nome é'+ MeuNome)
+d) console.log('Meu nome é'meuNome)
+
+*/
+
+
