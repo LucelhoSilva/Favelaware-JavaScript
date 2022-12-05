50d4f4e900729c743e619fd728b05edc9e537e45
Lucelho Cristiano <lucelhocristiano28@gmail.com>
2022-12-05 18:19:30 -0300

    Aula 1
    1. Imprima no console o seu nome e quantos anos tem ?
    
    2. Qual das opções abaixo é a correta para o uso do método .log()

---
+++ script1.js
@@ -0,0 +1,15 @@
+/* Imprima no console o seu nome e quantos anos tem */
+console.log("Digite o seu nome")
+console.log("Informe quantos anos tem")
+
+
+/* Qual das opções abaixo é a correta para o uso do método .log() */
+
+// a) console(log)
+// b) log.console(Ola)
+/* c) console.log('Meu nome é ...')  (essa e a opcao correta) */
+// d) console.print(Tenho 17 anos) 
+
+// Bonus: Para fazer comentarios em JavaScript usamos // para comentar linhas e /* */ para comentar blocos
+
+
