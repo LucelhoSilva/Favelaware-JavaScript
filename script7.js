9e40c07a982bd6862928e0032a14e0b9b9ca5732
Lucelho Cristiano <lucelhocristiano28@gmail.com>
2022-12-05 18:30:28 -0300

    Aula 7
    1. Crie um programa onde pede o usuário o seu nome, verifica se o nome está dentre todos os nomes dos alunos da sala, se falso pede para o usuário inserir um nome correto, se verdadeiro ele verifica qual case o nome é avaliado como verdadeiro, imprimindo para o usuário o nome do aluno e sua idade. (obs: use if…else e switch…case).
    
    2. Qual nome dado para o case padrão, identifique outra característica dele que o difere dos demais cases?
    
    3. Qual palavra-chave usamos após um case ser verificado como verdadeiro e sua ação foi executada?

---
+++ script7.js
@@ -0,0 +1,33 @@
+let listaDeNome = 'Lucas'
+
+switch (listaDeNome) {
+    case  'Lucas':
+        console.log ('nome Lucas esta na lista')
+        break;
+    case  'Pedro':
+        console.log('nome Pedro esta na lista')
+    case 'larissa':
+        console.log('nome Larissa esta na lista')
+    case 'Joao':
+        console.log('nome Joao esta na lista')
+    case 'Ana':
+        console.log('nome Ana esta na lista')
+        break;
+       default :
+       console.log('Item invalido')
+       break
+}
+
+/* 
+Qual nome dado para o case padão, identifique outra caracteristica dele que o difere dos demais cases ?
+
+switch
+
+*/
+
+/* 
+Qual palavra-chave usamos apos um case ser verificado como verdadeiro e sua ação foi executada ?
+
+break
+
+*/
