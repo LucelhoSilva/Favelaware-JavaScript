f5be4eccb97ff53b03f28a7ae5e8c819adaad9c7
Lucelho Cristiano <lucelhocristiano28@gmail.com>
2022-12-05 18:35:39 -0300

    Aula 9
    Crie uma função:
    
    Que calcula a soma de 2 números.
    Que calcula a divisão de 2 números.
    Que calcula a área de um triângulo.
    Que exiba uma mensagem de erro caso a idade seja menor que 18 anos.
    Que mostre sua altura em centímetros.
    Que mostre sua idade em anos, meses e depois em dias.
    Que mostre o tamanho do nome do usuário.

---
+++ script9.js
@@ -0,0 +1,39 @@
+/*somaNumeros=(numero1,numero2)=>{
+return numero1+numero2;
+}
+console.log('\n________'+'A soma de dois numeros é:________'+'\n                    '+somaNumeros(2,3))
+
+divideNumeros=(numero1,numero2)=>{
+return numero1/numero2;
+}
+console.log('\n________'+'A visisão de dois numeros é:________'+'\n              '+divideNumeros(2,3))
+
+areaTriangulo=(base,altura)=>{
+return base+altura/2;
+}
+console.log('\n________'+'A area do triangulo é:________'+'\n                  ' +areaTriangulo(2,3))
+
+
+tamanho=(altura=5)=>{
+return altura*100;
+}
+console.log('sua altura convertida para centimetros é ' +tamanho()+'cm')
+
+calculo1=(idade=22)=>{
+    return idade 
+    }
+calculo2=(idade=22)=>{
+    return idade *365
+    }
+calculo3=(idade=22)=>{
+    return idade*365/30
+}
+console.log('Voce tem : '+calculo1()+' anos')
+console.log('Sua idade em meses é: '+calculo3())
+console.log('Sua idade em dias é: '+calculo2())
+
+
+nome = "Cornelios"
+resultado = nome.length;
+
+console.log(resultado)*/
