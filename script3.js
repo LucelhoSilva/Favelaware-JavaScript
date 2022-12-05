2a5f0e3d2d020229eebfe11cf5671020746df01a
Lucelho Cristiano <lucelhocristiano28@gmail.com>
2022-12-05 18:22:32 -0300

    Aula 3
    1. Qual das alternativas a seguir é do tipo boolean
    
    2. Se imprimirmos um numero entre as aspas (' ' ou "") qual será seu tipo
    
    3. É certo dizer que o número 1.60 é do tipo Float ? Se não, qual o tipo do mesmo ?

---
+++ script3.js
@@ -0,0 +1,17 @@
+/* Qual das alternativas a seguir é do tipo boolean 
+ 
+a) "True"  (alternativa correta)
+b) "False" (alternativa correta)
+c) "Verdadeiro"
+d) "Falso"
+
+
+Se imprimirmos um numero entre as aspas (''ou "")  qual será seru tipo ?
+
+String
+
+É certo dizer que o um número 1.60 é do tipo Float ? Se não, qual o tipo do mesmo ?
+
+Não, ele é do tipo number
+
+*/
