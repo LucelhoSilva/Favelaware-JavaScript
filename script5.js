5dd576e0ae2ad4953152eb93c31beef08bda0700
Lucelho Cristiano <lucelhocristiano28@gmail.com>
2022-12-05 18:25:23 -0300

    Aula 5
    1.Imprima para o usuário uma frase elogiando seu colega do lado usando o literal de modelo.
    Lembre-se de atribuir o nome do seu colega a uma variável e usá-la no marcador de posição.
    
    2. Qual das opções abaixo é a incorreta sobre Interpolação?

---
+++ script5.js
@@ -0,0 +1,31 @@
+/*Imprima para o usuário uma frase elogiando seu colega do lado usando o literal de modelo. 
+Lembre-se de atribuir o nome do seu colega a uma variável e usá-la no marcador de  posição. 
+(  ${nomeColega} )*/
+
+const nome = 'Fulano';
+let idade = '(18)'
+
+const mensagem =
+`Olá me chamo ${nome} tenho ${idade} anos`;
+
+
+/*Imprima para o usuário uma frase elogiando seu colega do lado usando o literal de modelo. 
+Lembre-se de atribuir o nome do seu colega a uma variável e usá-la no marcador de  posição. 
+(  ${nomeColega} )*/
+
+
+let nomeAluno = 'Fulano'
+console.log(`Eu ${nomeAluno} sou aluno do jovem tech.`)
+
+
+/*
+Qual das opções abaixo é a incorreta sobre Interpolação? 
+
+a)Para fazer um Literal de Modelo usamos acento grave ( ` ) no início e no fim da mensagem.
+
+b)Para inserirmos uma variável dentro de um Literal de Modelo usamos o marcador de posição, 
+usando o sinal de dólar ( $ ) seguido por chaves ( { } ) e a variável dentro das chaves ( {variavelAqui} ).
+
+c)Usamos o operador de + para unirmos a variável com o texto.
+
+d)Podemos usar mais de um marcador de posição dentro do literal de modelo.*/ (INCORRETA)
